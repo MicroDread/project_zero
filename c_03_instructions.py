609294a41df3b663f16b6f4196ee0d14e80b4494
MicroDread <mutangak3654@masseyhigh.school.nz>
2026-03-04 14:39:15 +1300

    Add files via upload
    checkpoint 4 
    added ticket buyer list

---
+++ c_03_instructions.py
@@ -0,0 +1,69 @@
+# functions
+def make_statement(statement, decoration):
+    """Emphasises headings by adding decoration
+    at the start and end"""
+
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+def string_checker(questions, valid_answers=('yes', 'no'),
+                   num_letters=1):
+    """Checks that users enter the full word
+    or the first letter of a word from a list of valid responses"""
+
+    while True:
+
+        response = input(questions).lower()
+
+        for item in valid_answers:
+
+            # check if response is the entire word
+            if response == item:
+                return item
+
+            # check if the response is the entire word
+            if response == item:
+                return item
+
+            # check if its == item[0]
+            elif response == item[:num_letters]:
+                return item
+
+        print(f"please choose an option from {valid_answers}")
+
+def instructions():
+    make_statement( "instructions", "")
+
+    print("""
+    
+For each ticket holder enter ...
+- Their name 
+- Their age
+- The payment method (cash / credit)
+
+The program will record the ticket sale and calculate the 
+ticket cost (and the profit).
+
+Once you have either sold all of the tickets or entered the
+exit code ('xxx'), the program will display the ticket 
+ sales information and write the data to a text file.
+ 
+ it will also choose one lucky ticket holder who wins the 
+ draw (their ticket is free)
+
+
+""")
+
+
+# Main routine goes here
+
+make_statement( "Mini-Movie Fundraiser Program", "🍿")
+
+print()
+want_instructions = string_checker("Do you want to see the instructions? ")
+
+if want_instructions == "yes":
+    instructions()
+
+print()
+print("program continues...")
